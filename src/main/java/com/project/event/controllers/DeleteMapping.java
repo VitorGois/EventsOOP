1658b8b48fe0b0b5aa59bd2ed30a1623605fbe04
Otavio <otaviotppc@gmail.com>
2021-05-16 19:30:05 -0300

    Adding "Attendees" items

---
+++ src/main/java/com/project/event/controllers/DeleteMapping.java
@@ -0,0 +1,5 @@
+package com.project.event.controllers;
+
+public @interface DeleteMapping {
+
+}
---
+++ src/main/java/com/project/event/dtos/attendee/AttendeeUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.project.event.dtos.attendee;
+
+import lombok.Data;
+
+@Data
+public class AttendeeUpdateDTO {
+    private String email;
+
+}
